f3bbbb0503bfe7a8b6f9d399f9dd1a5c5b9c69f2
astroeng <dschacht@gmail.com>
2015-07-26 11:35:53 -0400

    Dummy Keys file.

---
+++ sparkfun_keys.h
@@ -0,0 +1,10 @@
+#ifndef SPARKFUN_KEYS_H
+#define SPARKFUN_KEYS_H
+
+/* Keys for updating the Sparkfun data stream. */
+/* These need to be set to something real.
+
+#define PUBLIC_KEY  xxxxxxxxxxxxxxxxxxxx
+#define PRIVATE_KEY yyyyyyyyyyyyyyyyyyyy
+
+#endif
