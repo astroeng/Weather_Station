d3af3e627ca17f99d5a5f1e406345677b4d084cb
Derek Schacht <dschacht@gmail.com>
2016-01-08 08:41:54 -0500

    Added a header to these files.

--- ethernet.h
+++ ethernet.h
@@ -1,3 +1,13 @@
+/* Derek Schacht
+ *  2016 01 07
+ *  License : Give me credit where it is due. 
+ *  Disclamer : I try and site code that I find on the internet but I am not perfect. If you find 
+ *              something that should be sited let me know and I will update my code.
+ *  Warranty : Absolutly None
+ *  
+ *  This header also applies to all previous commits. But, I reserve the right to modify this in the future.
+ */
+
 #ifndef ETHERNET_H
 #define ETHERNET_H
 
